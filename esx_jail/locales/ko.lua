a30aac1e7b0881c7559903ff00e130c5a7a6114b
JHyeok <dev.jhyeok@gmail.com>
2019-09-04 22:32:34 +0900

    Add ko.lua

---
+++ esx_jail/locales/ko.lua
@@ -0,0 +1,8 @@
+Locales ['ko'] = {
+	['blip_name']          = '감옥',
+	['judge']              = '판사',
+	['escape_attempt']     = '당신은 감옥을 탈출 할 수 없습니다!',
+	['remaining_msg']      = '감옥에서 풀려날 때까지 ~b~%s~s~초 남았습니다.',
+	['jailed_msg']         = '%s이(가) %s분 동안 감옥에 있습니다.',
+	['unjailed']           = '%s는 감옥에서 풀려났습니다!'
+}
